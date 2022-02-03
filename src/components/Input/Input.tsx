e808cd0cfbdad15ebd646b06148833bc0bcc72eb
egijasp <egija.srpukule@gmail.com>
2022-02-04 01:06:10 +0200

    first commit

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,24 @@
-import './Input.scss';
 import { ChangeEvent, FC } from 'react';
 
 type InputProps = {
   type: string,
   placeholder: string,
-  value?: string,
+  value: string,
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   className: string,
-  name?: string,
+  name: string,
+  min: string,
+  testId: string,
 }
 
 const Input:FC<InputProps> = ({
-  type, placeholder, value, onChange, className, name,
+  type, placeholder, value, onChange, className, name, min, testId,
 }) => (
   <input
     className={className}
+    data-testid={testId}
     type={type}
-    min="0"
+    min={min}
     placeholder={placeholder}
     value={value}
     onChange={onChange}
